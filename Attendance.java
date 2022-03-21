7624deb8b180f552f6de3a54323297fc547a01ed
BadGirl_QQ <73377113+Qrystal18@users.noreply.github.com>
2022-03-21 23:28:35 +0800

    Update Attendance.java

--- Attendance.java
+++ Attendance.java
@@ -1,14 +1,15 @@
 enum Attendance {
 
-  //enum fields
-  ATTENDED("ATTENDED"),ABSENTED("ABSENTED"),NAN("-");
+  	//enum fields
+  	ATTENDED("ATTENDED"),ABSENTED("ABSENTED"),NAN("-");
 	private final String attendance;
   
-  //constructor
+  	//constructor
 	private Attendance(String attendance) {
 		this.attendance = attendance;
 	}
   
+	//toString
 	public String getAttendance() {
 		return attendance;
 	}
