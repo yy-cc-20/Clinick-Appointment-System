b7b9589d8cf2ffc67fe993c1c26420aa1d595bf6
yy-cc-20 <65067887+yy-cc-20@users.noreply.github.com>
2022-03-21 22:51:27 +0800

    Update TimeSlot.java

--- TimeSlot.java
+++ TimeSlot.java
@@ -4,13 +4,13 @@ enum TimeSlot {
 	// "S1"       | "08:00AM - 08:30AM"
 	// toString() | getTime()
 
-	S1("08:00AM - 08:30AM"),
-	S2("08:30AM - 09:00AM"),
-	S3("09:00AM - 09:30AM"),
+	S1("0800AM"),
+	S2("0830AM"),
+	S3("0900AM"),
 	S4("
 	S5
 	// to be continue
-	S24("07:30PM - 08:00PM");
+	S24("07:30PM");
 
 	private final String time;
 
