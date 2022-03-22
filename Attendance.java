877b56f44d97094891dc01ab3ac07ec2df485e73
yy-cc-20 <65067887+yy-cc-20@users.noreply.github.com>
2022-03-22 14:03:09 +0800

    Update Attendance.java

--- Attendance.java
+++ Attendance.java
@@ -3,13 +3,14 @@ enum Attendance {
 	private final String attendance;
 	
   	//enum fields
-  	ATTENDED("ATTENDED"),ABSENT("ABSENT"),NAN("-");
+  	ATTENDED("ATTENDED"),
+	ABSENT("ABSENT"),
+	NAN("-");
   
   	//constructor
 	private Attendance(String attendance) {
 		this.attendance = attendance; }
   
-	//toString
 	public String getAttendance() {
 		return attendance; }
 }
