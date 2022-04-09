e9ccfd7fc59cc6f50ae6ef0c7696044e69d85cef
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-10 01:14:49 +0800

    Update Patient.java

--- entity/Patient.java
+++ entity/Patient.java
@@ -1,4 +1,9 @@
 public class Patient extends User {
+	private String ic;
+	private String phoneNo;
+	private String address;
+	private Appointment[] appointments;
+
 	public Patient(String userid, String username, String password) {
 		super(userid, username, password);
 	}
