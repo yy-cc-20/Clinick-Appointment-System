701961d253686631948dd2cb9267c5e9e184b46f
Yang Chu Yan <chuyanyang01@gmail.com>
2022-04-09 23:44:05 +0800

    .Create SetUpDatabase

---
+++ entity/CreateDatabase.java
@@ -0,0 +1,27 @@
+/*
+ * 1. import java.sql.*;
+ * 2. Load and register the driver
+ * 		2.1 Download the driver class
+ * 		Database | Driver class			 | Download 
+ * 		MySQL    | com.mysql.jdbc.Driver | mysql-connector-java-6.0.6.jar
+ * 
+ * 		2.2 Load and register the driver
+ * 		Class.forName("AJDBCDriverClass");
+ * 
+ * 3. Create connection
+ * 		String databaseURL = "jdbc:mysql://localhost/
+ * 		Connection conn = DriverManager.getConnection();
+ * 
+ * 4. Create a statement
+ * 5. Execute the query
+ * 6. Process the result
+ * 7. Close connection
+ */
+
+package entity;
+
+import java.sql.*;
+
+public class CreateDatabase {
+
+}
