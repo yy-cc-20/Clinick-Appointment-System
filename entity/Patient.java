7228f4a50179ce6520a6306a737733b33720c1b0
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-10 01:19:27 +0800

    Update Patient.java

--- entity/Patient.java
+++ entity/Patient.java
@@ -1,26 +1,34 @@
 public class Patient extends User {
-	private String ic;
-	private String phoneNo;
-	private String address;
-	private Appointment[] appointments;
+    private String ic;
+    private String phoneNo;
+    private String address;
+    private Appointment[] appointments;
 
-	public Patient(String userid, String username, String password) {
-		super(userid, username, password);
-	}
+    public Patient(String userid, String username, String password) {
+        super(userid, username, password);
+    }
 
-	public String getIc() {
-		return ic;
-	}
+    public Patient(String userid, String username, String password, String ic, String phoneNo, String address,
+                   Appointment[] appointments) {
+        super(userid, username, password);
+        this.ic = ic;
+        this.phoneNo = phoneNo;
+        this.address = address;
+    }
 
-	public String getPhoneNo() {
-		return phoneNo;
-	}
+    public String getIc() {
+        return ic;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getPhoneNo() {
+        return phoneNo;
+    }
 
-	public Appointment[] getAppointments() {
-		return appointments;
-	}
+    public String getAddress() {
+        return address;
+    }
+
+    public Appointment[] getAppointments() {
+        return appointments;
+    }
 }
