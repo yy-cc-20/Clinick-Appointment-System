f4fc1bb855a0c25be6de713aaa1804c49d614f04
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-10 01:38:06 +0800

    setup entity classes

--- entity/Branch.java
+++ entity/Branch.java
@@ -1,3 +1,55 @@
 public class Branch{
+    private String branchId;
+    private String branchName;
+    private String branchAddress;
+    private String receptionistId;
+    private String telNo;
 
+    public Branch(String branchId, String branchName, String branchAddress, String receptionistId, String telNo) {
+        this.branchId = branchId;
+        this.branchName = branchName;
+        this.branchAddress = branchAddress;
+        this.receptionistId = receptionistId;
+        this.telNo = telNo;
+    }
+
+    public String getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(String branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getBranchAddress() {
+        return branchAddress;
+    }
+
+    public void setBranchAddress(String branchAddress) {
+        this.branchAddress = branchAddress;
+    }
+
+    public String getReceptionistId() {
+        return receptionistId;
+    }
+
+    public void setReceptionistId(String receptionistId) {
+        this.receptionistId = receptionistId;
+    }
+
+    public String getTelNo() {
+        return telNo;
+    }
+
+    public void setTelNo(String telNo) {
+        this.telNo = telNo;
+    }
 }
--- entity/TimeSlot.java
+++ entity/TimeSlot.java
@@ -1,4 +1,3 @@
-package entity;
 
 enum TimeSlot {
 
