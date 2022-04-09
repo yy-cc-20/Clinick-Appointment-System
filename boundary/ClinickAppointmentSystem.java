633166ee167b3a1b187b438701e40597005e0242
y22ming <87871780+y22ming@users.noreply.github.com>
2022-04-09 14:57:54 +0800

    Update ClinickAppointmentSystem.java

--- boundary/ClinickAppointmentSystem.java
+++ boundary/ClinickAppointmentSystem.java
@@ -5,6 +5,38 @@ import boundary.*;
 
 public class ClinickAppointmentSystem {
 	public static void main(String[] args) {
+		
+		// instantiating the dataList object
+		IDataStore dataList = new DataList();
+
+		// instantiating the controllers
+		LoginController loginController = new LoginController();
+		MakeAppointmentController makeAppointmentController = new MakeAppointmentController();
+		ManageAppointmentController manageAppointmentController = new ManageAppointmentController();
+		ManageAccountController manageAccountController = new ManageAccountController();
+		ManagePatientController managePatientController = new ManagePatientController();
+
+		// setting the controllers to the dataList object
+		loginController.setDataLists(dataLists);
+		makeAppointmentController.setDataLists(dataLists);
+		manageAppointmentController.setDataLists(dataLists);
+		manageAccountController.setDataLists(dataLists);
+		managePatientController.setDataLists(dataLists);
+
+		// instantiating the user interfaces
+		LoginUI loginInterface = new LoginUI();
+		MakeAppointmentUI makeAppointmentInterface = new MakeAppointmentUI();
+		ManageAppointmentUI manageAppointmentInterface = new ManageAppointmentUI();
+		ManageAccountUI manageAccountInterface = new ManageAccountUI();
+		ManagePatientUI managePatientInterface = new ManagePatientUI();
+
+		// setting the user interfaces to their controllers
+		loginInterface.setController(loginController);
+		makeAppointmentInterface.setController(makeAppointmentController);
+		manageAppointmentInterface.setController(manageAppointmentController);
+		manageAccountInterface.setController(manageAccountController);
+		managePatientInterface.setController(managePatientController);
+		
 		User systemUser = new LoginView().login();
 		
 		int eventNo; // the action that user wants to perform
