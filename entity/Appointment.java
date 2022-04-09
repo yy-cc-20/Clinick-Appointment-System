0d36e59672ab83120cf9e7e7a31e9e6b56d26cbf
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-10 01:52:56 +0800

    Update Appointment.java

--- entity/Appointment.java
+++ entity/Appointment.java
@@ -1,10 +1,20 @@
 package entity;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Appointment {
+    private static final String pattern = "dd/MM/yyyy"; // date format
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
     private String appointmentId;
     private LocalDate appointmentDate;
     private Attendance attendance;
     private TimeSlot[] timeSlot;
+
+    public Appointment(String appointmentId, String appointmentDate, String attendance, String[] timeSlot) {
+        this.appointmentId = appointmentId;
+        this.appointmentDate = LocalDate.parse(appointmentDate, formatter);
+        this.attendance = attendance;
+        this.timeSlot = timeSlot;
+    }
 }
