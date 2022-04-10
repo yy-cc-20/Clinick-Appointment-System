eeac39f72b93eb26c6b20347da299f011b3bd8e7
y22ming <olivia.ong22@gmail.com>
2022-04-10 19:34:59 +0800

    Update ManageAppointmentUI.java

--- src/boundary/ManageAppointmentUI.java
+++ src/boundary/ManageAppointmentUI.java
@@ -9,28 +9,24 @@ import entity.Attendance;
 public class ManageAppointmentUI {
 
     private final ManageAppointmentController controller = new ManageAppointmentController();
-    private List<Appointment> selectedAppointment;
+    private static Appointment selectedAppointment;
 
     public void searchAppointmentToModify() {
         String appointmentId = KeyboardInput.askString("appointment ID");
-        selectedAppointment = controller.searchAppointment(1, appointmentId);
+        List<Appointment> selectedAppointments = controller.searchAppointment(1, appointmentId);
 
-        if (selectedAppointment.size() == 0) {
+        if (selectedAppointments.size() == 0) {
             System.out.println("No such appointment found.");
         } else {
-            for (Appointment theAppointment : selectedAppointment) {
-                MakeAppointmentUI.displayAppointment(theAppointment);
-            }
+            selectedAppointment = selectedAppointments.get(0);
+            MakeAppointmentUI.displayAppointment(selectedAppointment);
         }
     }
 
     public void updateAppointment() {
         searchAppointmentToModify();
-        // call viewSlots?
-        // MakeAppointmentUI.viewSlots();
 
         if (KeyboardInput.askBoolean("Update appointment")) {
-            // todo
             System.out.println("Appointment updated.");
         }
     }
@@ -39,8 +35,7 @@ public class ManageAppointmentUI {
         searchAppointmentToModify();
         if (KeyboardInput.askBoolean("Cancel appointment")) {
             controller.cancelAppointment(selectedAppointment);
-            // todo
-            System.err.println("Appointment cancelled.");
+            System.out.println("Appointment cancelled.");
         }
     }
 
@@ -48,9 +43,8 @@ public class ManageAppointmentUI {
         searchAppointmentToModify();
         // KeyboardInput.askAttendance;
         Attendance attendance = Attendance.askAttendance();
-        selectedAppointment.get(0).setAttendance(attendance);
-        for (Appointment theAppointment : selectedAppointment) {
-            MakeAppointmentUI.displayAppointment(theAppointment);
-        }
+        selectedAppointment.setAttendance(attendance);
+        MakeAppointmentUI.displayAppointment(selectedAppointment);
     }
+    
 }
