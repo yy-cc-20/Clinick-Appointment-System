483f900c5f6d333a6afb7535b82c394ad460bd0d
sunshuaiii <77444967+sunshuaiii@users.noreply.github.com>
2022-04-10 09:54:00 +0800

    class setup

--- src/entity/Doctor.java
+++ src/entity/Doctor.java
@@ -4,4 +4,6 @@ public class Doctor extends User {
 	public Doctor(String userid, String username, String password) {
 		super(userid, username, password);
 	}
+
+	public Doctor(){}
 }
--- src/entity/Receptionist.java
+++ src/entity/Receptionist.java
@@ -4,4 +4,6 @@ public class Receptionist extends User {
 	public Receptionist(String userid, String username, String password) {
 		super(userid, username, password);
 	}
+
+	public Receptionist(){}
 }
