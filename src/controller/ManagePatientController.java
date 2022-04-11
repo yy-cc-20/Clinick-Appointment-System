1b7096edb013628726d0748c11b2161432674a9d
Bruce Wayne <73377113+Qrystal18@users.noreply.github.com>
2022-04-12 03:50:56 +0800

    Update ManagePatientController.java

--- src/controller/ManagePatientController.java
+++ src/controller/ManagePatientController.java
@@ -1,12 +1,66 @@
 package controller;
 
-import entity.Patient;
+import java.sql.*;
+import database.DatabaseConnection;
+import entity.*;
 
 public class ManagePatientController {
+	
+	private static ManagePatientController instance;
+	private Statement st;
+	private ResultSet rs;
+	private String sql;
+	
+	public ManagePatientController() {
+		try {
+			st = DatabaseConnection.getConnection().createStatement();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static ManagePatientController getInstance() {
+		if (instance == null)
+			new ManagePatientController();
+		return instance;
+	}
+	
     public Patient searchPatient(String patientIc) {
+    	
+    	sql = "SELECT * FROM PATIENT WHERE patientIc = " + patientIc;
+    	try {
+    	     rs = st.executeQuery(sql);
+    	    while(rs.next()) {
+    	    	Patient patient = new Patient();
+                patient.setName(1, rs.getString(1));
+                patient.setUserId(2, rs.getString(2));
+                patient.setPhoneNo(3,rs.getString(3));
+                patient.setAddress(4,rs.getString(4));
+                patient.setIc(patientIc);
+    	    	}
+  
+    	} catch (SQLException e) {
+    	    	e.printStackTrace();
+    	    }
+
         return new Patient();
     }
 
     public void addPatient(String name, String patientIc) {
+    	
+    	sql = "INSERT INTO PATIENT('NAME','PATIENTIC')"
+    			+ "VALUES('" + name + "', '" + patientIc +");";
+    	try {
+    	     rs = st.executeQuery(sql);
+    	    while(rs.next()) {
+    	    	Patient patient = new Patient();
+                patient.setName(1, rs.getString(1));
+                patient.setIc(2, rs.getString(2));
+    	    	}
+  
+    	} catch (SQLException e) {
+    	    	e.printStackTrace();
+    	    }
     }
 }
+
