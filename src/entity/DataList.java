08902fd95dcd17272def315966396a196d82951e
y22ming <olivia.ong22@gmail.com>
2022-04-13 00:49:56 +0800

    Update DataList.java

--- src/entity/DataList.java
+++ src/entity/DataList.java
@@ -1,11 +1,15 @@
 package entity;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.sql.PreparedStatement;
+import java.sql.Date;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import database.DatabaseConnection;
@@ -25,13 +29,15 @@ public class DataList implements IDataStore {
 	private List<Branch> branchList;
 	private List<Service> serviceList;
 
+	private Connection con;
 	private Statement st;
 	private ResultSet rs;
 
 	// Private constructor for singleton
 	private DataList() {
 		try {
-			st = DatabaseConnection.getConnection().createStatement();
+			con = DatabaseConnection.getConnection();
+			st = con.createStatement();
 			doctorList = new ArrayList<>();
 			patientList = new ArrayList<>();
 			receptionistList = new ArrayList<>();
@@ -51,6 +57,35 @@ public class DataList implements IDataStore {
 		return instance;
 	}
 
+	public List<Appointment> getAppointmentList(String query, String column, String data) {
+		try {
+			// e.g., query = "filter" column = "id" data = "1"
+			// e.g., query = "sort" column = "id" data = "asc"
+			if (query == null)
+				rs = st.executeQuery("SELECT * FROM appointment ORDER BY id;");
+			else if (query.equalsIgnoreCase("filter"))
+				rs = st.executeQuery("SELECT * FROM appointment WHERE " + column + " = " + data + ";");
+			else if (query.equalsIgnoreCase("sort"))
+				rs = st.executeQuery("SELECT * FROM appointment ORDER BY " + column + " " + data + ";");
+
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				Date date = rs.getDate("date");
+				String attendance = rs.getString("attendance");
+				int startSlot = rs.getInt("startSlot");
+				int patientId = rs.getInt("patientId");
+				int allocationId = rs.getInt("allocationId");
+				Appointment appointment = new Appointment(id, date.toString(), patientId,
+						allocationId, attendance, startSlot);
+				appointmentList.add(appointment);
+			}
+			System.out.println("appointmentList " + appointmentList.size());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return appointmentList;
+	}
+
 	// Return branch objects of the specified ids
 	public List<Branch> getBranchesById(List<Integer> ids) {
 		List<Branch> branchResults = new ArrayList<>();
@@ -63,8 +98,6 @@ public class DataList implements IDataStore {
 
 	public List<Branch> getBranchList(String query, String column, String data) {
 		try {
-			// e.g., query = "filter" column = "id" data = "1"
-			// e.g., query = "sort" column = "id" data = "asc"
 			if (query == null)
 				rs = st.executeQuery("SELECT * FROM branch ORDER BY id;");
 			else if (query.equalsIgnoreCase("filter"))
@@ -143,9 +176,16 @@ public class DataList implements IDataStore {
 		return allocationList;
 	}
 
-	public List<Patient> getPatientList() {
+	public List<Patient> getPatientList(String query, String column, String data) {
 		try {
 			rs = st.executeQuery("SELECT * FROM patient ORDER BY id;");
+			if (query == null)
+				rs = st.executeQuery("SELECT * FROM patient ORDER BY id;");
+			else if (query.equalsIgnoreCase("filter"))
+				rs = st.executeQuery("SELECT * FROM patient WHERE " + column + " = " + data + ";");
+			else if (query.equalsIgnoreCase("sort"))
+				rs = st.executeQuery("SELECT * FROM patient ORDER BY " + column + " " + data + ";");
+
 			while (rs.next()) {
 				int id = rs.getInt("id");
 				String name = rs.getString("name");
@@ -156,7 +196,7 @@ public class DataList implements IDataStore {
 				Patient patient = new Patient(id, name, password, ic, phone, address);
 				patientList.add(patient);
 			}
-			System.out.println("patientList "+patientList.size());
+			System.out.println("patientList " + patientList.size());
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
@@ -173,32 +213,80 @@ public class DataList implements IDataStore {
 				Receptionist receptionist = new Receptionist(id, name, password);
 				receptionistList.add(receptionist);
 			}
-			System.out.println("receptionistList "+receptionistList.size());
+			System.out.println("receptionistList " + receptionistList.size());
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return receptionistList;
 	}
 
-	public List<Appointment> getAppointmentList() {
+	public void addAppointment(String date, String attendance, int startSlot, int patientId, int allocationId) {
 		try {
-			rs = st.executeQuery("SELECT * FROM appointment ORDER BY id;");
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				Date date = rs.getDate("date");
-				String attendance = rs.getString("attendance");
-				int startSlot = rs.getInt("startSlot");
-				int patientId = rs.getInt("patientId");
-				int allocationId = rs.getInt("allocationId");
-				Appointment appointment = new Appointment(id, date.toString(), patientId,
-						allocationId, attendance, startSlot);
-				appointmentList.add(appointment);
-			}
-			System.out.println("appointmentList " + appointmentList.size());
+			st.executeUpdate("INSERT IGNORE INTO apointment (date, attendance, startSlot, patientId, allocationId) " +
+					"VALUES ('" + date + "','" + attendance + "','" + startSlot + "','" + patientId + "','"
+					+ allocationId + "')");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return appointmentList;
 	}
 
+	// provide the patient's basic information to add into database
+	public void addPatientPartial(String name, String ic, String password) {
+		try {
+			st.executeUpdate("INSERT IGNORE INTO patient (name, ic, password) " +
+					"VALUES ('" + name + "','" + ic + "','" + password + "')");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// provide the patient's full information to add into database
+	public void addPatientFull(String name, String ic, String phone, String address, String password) {
+		try {
+			st.executeUpdate("INSERT IGNORE INTO patient (name, ic, phone, address, password) " +
+					"VALUES ('" + name + "','" + ic + "','" + phone + "','" + address + "','" + password + "')");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// provide the appointmentId to update, with the new date and startSlot
+	public void updateAppointmentTime(int id, String date, int startSlot) {
+		String query = ("UPDATE appointment SET date=?, startSlot=?, WHERE id=?");
+		try (PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setInt(3, id);
+			pstmt.setDate(1, new Date(new SimpleDateFormat("dd-MM-yyyy").parse(date).getTime()));
+			pstmt.setInt(2, startSlot);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// provide the appointmentId to update, with the updated attendance record
+	public void updateAppointmentAttendance(int id, String attendance) {
+		String query = ("UPDATE appointment SET attendance=? WHERE id=?");
+		try (PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setInt(2, id);
+			pstmt.setString(1, attendance);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// provide the appointmentId to update to cancel the appointment
+	public void cancelAppointment(int id) {
+		// TODO : cancel the appointment
+		String query = ("DELETE FROM appointment WHERE id=?");
+		try (PreparedStatement pstmt = con.prepareStatement(query)) {
+			pstmt.setInt(1, id);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
