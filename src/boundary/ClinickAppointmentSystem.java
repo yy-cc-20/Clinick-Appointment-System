b070906b2e7be45ef19959b0c3e769856ec00936
Yang Chu Yan <chuyanyang01@gmail.com>
2022-04-12 20:30:25 +0800

    main method

--- src/boundary/ClinickAppointmentSystem.java
+++ src/boundary/ClinickAppointmentSystem.java
@@ -27,51 +27,80 @@ import entity.*;
 
 public class ClinickAppointmentSystem {
 	public static void main(String... args) throws SQLException {
-		
-		// the test data is created in DataList
-		// role: doctor
-		// userid: 1
-		// username: username
-		// password: password
-
-		ConsoleUI.displaySystemName("Clinic Booking System");
-		User systemUser = new LoginUI().login(); // Suspend the user to login for 10 seconds after 3 failed login attempts
-		// From systemUser can know the username, id, password, user type
-
+		/*
 		// instantiating the user interfaces
 		MakeAppointmentUI makeAppointmentInterface = new MakeAppointmentUI(systemUser);
 		ManageAppointmentUI manageAppointmentInterface = new ManageAppointmentUI();
 		ManagePatientUI managePatientInterface = new ManagePatientUI();
-
-		ConsoleUI.clearScreen();
-
-		int choiceNo; // the action that user wants to perform
-		final int beginChoiceNo = 1; 
-		final int endChoiceNo = 3;
-		int i =1;
+		*/
+		boolean toExit = false;
+		
 		while (true) {
-			ConsoleUI.displaySystemName("System Name");
-			//ConsoleUI.displayMenu(); // need to change the menu
-			choiceNo = ConsoleInput.askChoice(beginChoiceNo, endChoiceNo, "Your choice");
-
-			switch (choiceNo) {
-				case 1 -> {
-					// Just to test the method, you may change the position of the code
-					ConsoleUI.displayFunctionName("Account Setting");
-					new ManageAccountUI(systemUser).changePassword();
-				}
-				case 2 -> {// Modify Account Info
-						ConsoleUI.displayFunctionName("View Services and Time Slots");
-						ViewSlotsUI.getInstance().viewSlots();
-				}
-				case 3 -> { // logout and exit the program
-					ConsoleUI.displayFunctionName(" Program Stopped ");
-					SingletonScanner.scanner.close();
-					DatabaseConnection.closeConnection(); /** @throws SQLException */
-					System.exit(0);
-				}
-			}
+			ConsoleUI.displaySystemName("Clinic Booking System");
+			User systemUser = new LoginUI().login(); // Suspend the user to login for 10 seconds after 3 failed login attempts
+			// From systemUser can know the username, id, password, user type
+	
 			ConsoleUI.clearScreen();
+			
+			if (systemUser instanceof Receptionist) 
+				toExit = startReceptionistView(systemUser);
+			else if (systemUser instanceof Doctor) 
+				toExit = startDoctorView(systemUser);
+			else if (systemUser instanceof Patient) 
+				toExit = startPatientView(systemUser);
+			
+			ConsoleUI.clearScreen();
+			
+			if (toExit)
+				System.exit(0);
 		}
 	}
+		
+	/** @return true to logout, false to exit application */
+	static boolean startReceptionistView(User systemUser) {
+		
+		
+	}
+	
+	/** @return true to logout, false to exit application */
+	static boolean startDoctorView(User systemUser) {
+		
+	}
+	
+	/** @return true to logout, false to exit application */
+	static boolean startPatientView(User systemUser) {
+		
+	}
+	
+	/*
+
+	int choiceNo; // the action that user wants to perform
+	final int beginChoiceNo = 1; 
+	final int endChoiceNo = 3;
+	
+	while (true) {
+		ConsoleUI.displaySystemName("System Name");
+		//ConsoleUI.displayMenu(); // need to change the menu
+		choiceNo = ConsoleInput.askChoice(beginChoiceNo, endChoiceNo, "Your choice");
+
+		switch (choiceNo) {
+			case 1 -> {
+				// Just to test the method, you may change the position of the code
+				ConsoleUI.displayFunctionName("Account Setting");
+				new ManageAccountUI(systemUser).changePassword();
+			}
+			case 2 -> {// Modify Account Info
+					ConsoleUI.displayFunctionName("View Services and Time Slots");
+					ViewSlotsUI.getInstance().viewSlots();
+			}
+			case 3 -> { // logout and exit the program
+				ConsoleUI.displayFunctionName(" Program Stopped ");
+				SingletonScanner.scanner.close();
+				DatabaseConnection.closeConnection();  @throws SQLException
+				System.exit(0);
+			}
+		}
+		ConsoleUI.clearScreen();
+	}
+	*/
 }
